8a12702810a8c3bcefcd3171999cbd704ec37b1a
RDJinTropicThunder <nevolve27@gmail.com>
2022-10-27 12:17:02 +0700

    Catalog

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -3,5 +3,79 @@
 @section('title', 'Catalog')
 
 @section('content')
-    <h1>Catalog</h1>
+
+    {{-- masthead --}}
+    <div class="bg-filled bg-no-repeat bg-bottom pt-32 pb-64 relative" style="background-image: url('img/masthead.jpg')">
+        <h1 class="text-white text-4xl font-bold pl-4 underline decoration-orange-600 absolute bottom-0 left-0 h-24">All Miniatures</h1>
+    </div>
+
+
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 content-center bg-gradient-to-br from-gray-100 via-gray-300 to-gray-500">
+        
+    <div class="max-w-sm">
+        <a href="#">
+            <img class="rounded-t-lg" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/24/367aa915-8b2d-4162-9687-ee7fa0f0c2cc.jpg" alt="">
+        </a>
+        <div class="pt-3 pl-2 content-start">
+            <hr class="my-1 mr-2 w-auto h-1 bg-orange-600 rounded border-0 md:my-2">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-900">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </div>
+
+    <div class="max-w-sm">
+        <a href="#">
+            <img class="rounded-t-lg" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/24/367aa915-8b2d-4162-9687-ee7fa0f0c2cc.jpg" alt="">
+        </a>
+        <div class="pt-3 pl-2 content-start">
+            <hr class="my-1 mr-2 w-auto h-1 bg-orange-600 rounded border-0 md:my-2">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-900">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </div>
+
+    <div class="max-w-sm">
+        <a href="#">
+            <img class="rounded-t-lg" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/24/367aa915-8b2d-4162-9687-ee7fa0f0c2cc.jpg" alt="">
+        </a>
+        <div class="pt-3 pl-2 content-start">
+            <hr class="my-1 mr-2 w-auto h-1 bg-orange-600 rounded border-0 md:my-2">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-900">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </div>
+
+    <div class="max-w-sm">
+        <a href="#">
+            <img class="rounded-t-lg" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/24/367aa915-8b2d-4162-9687-ee7fa0f0c2cc.jpg" alt="">
+        </a>
+        <div class="pt-3 pl-2 content-start">
+            <hr class="my-1 mr-2 w-auto h-1 bg-orange-600 rounded border-0 md:my-2">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-900">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </div>
+
+    <div class="max-w-sm">
+        <a href="#">
+            <img class="rounded-t-lg" src="https://images.tokopedia.net/img/cache/500-square/VqbcmM/2022/7/24/367aa915-8b2d-4162-9687-ee7fa0f0c2cc.jpg" alt="">
+        </a>
+        <div class="pt-3 pl-2 content-start">
+            <hr class="my-1 mr-2 w-auto h-1 bg-orange-600 rounded border-0 md:my-2">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-900">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </div>
+
+    </div>
 @endsection
