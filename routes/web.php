bf62477feaa1678870574c8b1b927a8eed83b7d8
RDJinTropicThunder <nevolve27@gmail.com>
2022-11-09 17:37:43 +0700

    fix more routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\NewProductController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,15 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome', [
-        'title' => 'Home'
-    ]);
-});
+
 
 Route::get('/new', [NewProductController::class, 'index']);
 
 Route::get('/catalog', [ProductController::class, 'index']);
+
+Route::get('/', [Controller::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
